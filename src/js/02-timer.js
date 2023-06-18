d2e998ed916eabbc33c3bb73d0b38e7c45506fa7
KwintDimon <121832419+KwintDimon@users.noreply.github.com>
2023-06-19 00:35:55 +0300

    finished task09-2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,69 +1,96 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
 
-//  const startBtn=document.querySelector('button')
-//   const dateTimePicker=document.querySelector('input')
-// console.log(startBtn,dateTimePicker)
+const inputRef = document.querySelector('#datetime-picker');
+const btnStart = document.querySelector('button[data-start]');
+const timerDayRef = document.querySelector('span[data-days]');
+const timerHoursRef = document.querySelector('span[data-hours]');
+const timerMinutesRef = document.querySelector('span[data-minutes]');
+const timerSecondsRef = document.querySelector('span[data-seconds]');
+
+let selectedTimeMs = null;
+let timerInterval;
+btnStart.disabled = true;
 
 
-// dateTimePicker.addEventListener('change', (e) => {
-//   const date = new Date(e.target.value)
-//   if (date.getTime() > Date.now()) {
-//   alert();
-//   }
-//   console.log(e.target.value)
-// })
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onOpen() {
+    timerInterval && clearInterval(timerInterval);
 
-
-// const timer = {
-//   start() {
-//     const startTime = Date.now();
-//     setInterval(() => {
-//       const currentTime = Date.now();
-//       const deltaTime = currentTime - startTime;
-//       const  { hours, mins, secs }=getTimeComponents(deltaTime)
-//       // console.log(startTime)
-//       // console.log(currentTime)
-//       // console.log(currentTime - startTime);
-//       console.log(`${hours}:${mins}:${secs}`)
-      
-//     }, 1000);
-//   }
-// };
-// timer.start();
-
-// startBtn.addEventListener('click', () => {
-//   timer.start();
-// });
-
-// function updateClockface({ hours, mins, secs }) {
-//   refs.updateClockface.textContent=`${hours}:${mins}:${secs}`
-// };
-// function pad(value) {
-//   return String(value).padStart(2, '0');
-// };
-// function getTimeComponents(time) {
-//   const hours =
-//     pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-//   ;
-//   const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60 )));
-//   const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-//   return { hours, mins, secs };
-// }
-
-//!!!Практика
-let counter = 0
-function showTime() {
+     timerDayRef.textContent = '00';
+  timerHoursRef.textContent = '00';
+  timerMinutesRef.textContent = '00';
+    timerSecondsRef.textContent = '00';
+       
+  },
+  onClose(selectedDates) {
+    if (selectedDates[0] < new Date()) {
+      alert("Please choose a date in the future")
+    }
+    else {
+      btnStart.disabled = false; 
+      selectedTimeMs = selectedDates[0].getTime();
+      console.log(selectedTimeMs);
+    }
   
- let timeId= setTimeout(() => {
-   const date = new Date()
-   counter +=1
-    showTime()
-    console.log(date.toLocaleTimeString())
-    
+  },
+};
+
+let selectedDay = flatpickr(inputRef, options);
+btnStart.addEventListener('click', onBtnStartClick);
+function onBtnStartClick(event) {
+  btnStart.disabled = true;
+
+
+  let deltaTime =selectedTimeMs - Date.now();
+ console.log(deltaTime);
+  
+
+  
+  timerInterval = setInterval(() => {
+
+const { days, hours, minutes, seconds } = convertMs(deltaTime);
+
+  timerDayRef.textContent = addLeadingZero(days);
+  timerHoursRef.textContent = addLeadingZero(hours);
+  timerMinutesRef.textContent = addLeadingZero(minutes);
+    timerSecondsRef.textContent = addLeadingZero(seconds);
+    deltaTime -= 1000;
   }, 1000)
-    if (counter === 5) {
-      clearTimeout(timeId)
-     
-  }
+  console.log(timerInterval)
 }
 
-showTime()
+
+function addLeadingZero(value){
+  return String(value).padStart(2, '0');
+}
+
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+flatpickr(inputRef, options);
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,35 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
+
+//!!!!!!!!!Репета
+const makeOrger = dish => {
+  const DELAY = 1000;
+  return new Promise((resolve, reject) => {
+    const passed = Math.random() > 0.5;
+
+    setTimeout(() => {
+      if (passed) {
+        resolve('your order')
+      }
+      reject('no finished your order')
+    },DELAY)
+  });
+};
+makeOrger('bake').then(onMakeOrderSuccess).catch(onMakeOrderError);
+
+function onMakeOrderSuccess(result) {
+  console.log('onMakeOrderSuccess');
+  console.log(result);
 }
+
+function onMakeOrderError(error) {
+  console.log('onMakeOrderError');
+  console.log(error);
+}
+
