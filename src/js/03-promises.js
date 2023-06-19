fcce0e5950ef4431c63ed7eb8d2ee094ccb6480f
KwintDimon <121832419+KwintDimon@users.noreply.github.com>
2023-06-19 23:35:26 +0300

    finished task09-3js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,35 +1,44 @@
-// function createPromise(position, delay) {
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     // Fulfill
-//   } else {
-//     // Reject
-//   }
-// }
+  
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-//!!!!!!!!!Репета
-const makeOrger = dish => {
-  const DELAY = 1000;
+// 03-promises.js
+document.querySelector('.form').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const delayInput = document.querySelector('input[name="delay"]');
+  const stepInput = document.querySelector('input[name="step"]');
+  const amountInput = document.querySelector('input[name="amount"]');
+
+  const firstDelay = parseInt(delayInput.value);
+  const step = parseInt(stepInput.value);
+  const amount = parseInt(amountInput.value);
+
+  for (let i = 0; i < amount; i++) {
+    const delay = firstDelay + i * step;
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+});
+
+function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
-    const passed = Math.random() > 0.5;
-
-    setTimeout(() => {
-      if (passed) {
-        resolve('your order')
-      }
-      reject('no finished your order')
-    },DELAY)
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      setTimeout(() => {
+        resolve({ position, delay });
+      }, delay);
+    } else {
+      setTimeout(() => {
+        reject({ position, delay });
+      }, delay);
+    }
   });
-};
-makeOrger('bake').then(onMakeOrderSuccess).catch(onMakeOrderError);
-
-function onMakeOrderSuccess(result) {
-  console.log('onMakeOrderSuccess');
-  console.log(result);
 }
 
-function onMakeOrderError(error) {
-  console.log('onMakeOrderError');
-  console.log(error);
-}
+
 
